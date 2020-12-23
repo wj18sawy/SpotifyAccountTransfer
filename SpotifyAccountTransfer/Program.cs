c1ce9f23269e3893a0ff8c29eab8288b688b8914
Wyatt Sawyer <wyatt.sawyer@conduent.com>
2020-12-22 20:25:18 -0500

    Setting up configuration correctly for appsettings.json

--- SpotifyAccountTransfer/Program.cs
+++ SpotifyAccountTransfer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SpotifyAPI.Web;
+using System;
 
 namespace SpotifyAccountTransfer
 {
@@ -7,6 +9,19 @@ namespace SpotifyAccountTransfer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! This is the development branch.");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+
+            string token = config["Token"];
+            var spotify = new SpotifyClient(token);
+
+
+            var allMyTracks = spotify.Library.GetTracks().Result;
+            var track = spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+            Console.WriteLine(track.Result.Name);
         }
     }
 }
